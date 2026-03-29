aa08d64ce800a976ead2e6a26521f405fbf18a11
nickmsk9 <94682037+nickmsk9@users.noreply.github.com>
2026-03-29 13:43:14 +0300

    •	AJAX теперь шлёт POST, как и ждёт user.php 	•	параметр сменён с c на act 	•	добавил dataType: 'html' 	•	если user не передан, таб сразу покажет понятную ошибку вместо кривого запроса 	•	если сервер вернёт текст ошибки, он теперь покажется внутри таба, а не потеряется

--- www/js/user.js
+++ www/js/user.js
@@ -3,14 +3,14 @@
  */
 (function($) {
     $(function() {
-        var $body    = $('#body'),
+        var $body    = $('#user_tab_body'),
             $loading = $('#loading'),
-            user     = $body.attr('user') || '';
+            user     = $body.data('user') || $body.attr('data-user') || '';
 
         // Loading indicator HTML
         var loadingHtml = '<img src="pic/upload.gif" alt="Загрузка...">';
 
-        // Apply zebra striping to table rows inside #body
+        // Apply zebra striping to table rows inside #user_tab_body
         function applyZebra() {
             $body.find('tr').css('backgroundColor', '');
             $body.find('tr:even').css('backgroundColor', '#EEEEEE');
@@ -18,13 +18,22 @@
 
         // Bust image cache by appending timestamp
         function bustImages() {
-            var ts = Date.now();
+            var ts = new Date().getTime();
             $body.find('img').each(function() {
-                var $img = $(this);
+                var $img = $(this),
+                    src = $img.attr('src') || '',
+                    sep;
+
+                if (!src) {
+                    return;
+                }
+
                 // Remove any existing cache-buster parameter
-                var src = $img.attr('src').replace(/([?&])_=\d+/, '');
+                src = src.replace(/([?&])_=\d+/, '');
+
                 // Determine separator for new parameter
-                var sep = src.indexOf('?') > -1 ? '&' : '?';
+                sep = src.indexOf('?') > -1 ? '&' : '?';
+
                 // Reapply src with new cache-buster, preserving other parameters
                 $img.attr('src', src + sep + '_=' + ts);
             });
@@ -36,21 +45,37 @@
             $('.tab').removeClass('active');
             $('.tab#' + act).addClass('active');
 
+            if (!user) {
+                $body.html('<p>Ошибка: не передан ID пользователя.</p>');
+                return;
+            }
+
             // Show loading spinner
             $loading.html(loadingHtml);
 
             // AJAX fetch
             $.ajax({
                 url: 'user.php',
-                data: { user: user, c: act },
+                type: 'POST',
+                dataType: 'html',
+                data: { user: user, act: act },
                 cache: false,
                 success: function(response) {
                     $body.html(response);
                     bustImages();
                     applyZebra();
                 },
-                error: function() {
-                    $body.html('<p>Ошибка при загрузке данных.</p>');
+                error: function(xhr, status, error) {
+                    var msg = 'Ошибка при загрузке данных.';
+
+                    if (xhr && xhr.responseText) {
+                        $body.html(xhr.responseText);
+                    } else {
+                        if (error) {
+                            msg += ' ' + error;
+                        }
+                        $body.html('<p>' + msg + '</p>');
+                    }
                 },
                 complete: function() {
                     $loading.empty();
@@ -58,8 +83,8 @@
             });
         }
 
-        // Bind click on tabs
-        $(document).on('click', '.tab', function(e) {
+        // Bind click on tabs (jQuery 1.4.2 compatible)
+        $('.tab').live('click', function(e) {
             e.preventDefault();
             var act = this.id;
             if (!$(this).hasClass('active')) {
@@ -72,9 +97,9 @@
             $init = $('.tab.active');
 
         if ($init.length) {
-            loadTab($init.first().attr('id'));
+            loadTab($init.eq(0).attr('id'));
         } else if ($tabs.length) {
-            loadTab($tabs.first().attr('id'));
+            loadTab($tabs.eq(0).attr('id'));
         }
     });
 })(jQuery);
