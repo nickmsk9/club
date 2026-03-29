295f302658445de0f4d2c13d78bb76ff38a835c2
Nick Sevalnev <94682037+nickmsk9@users.noreply.github.com>
2026-03-29 14:44:13 +0300

    Исправлен файл uncheck

--- www/uncheck.php
+++ www/uncheck.php
@@ -1,11 +1,11 @@
 <?php
 require_once("include/bittorrent.php");
-global $memcache_obj;
+global $memcache_obj, $lang;
 dbconn();
 loggedinorreturn();
 
 header ("Content-Type: text/html; charset=" . $lang['language_charset']);
-$torrent = (int) $_POST["torrent"];
+$torrent = isset($_POST["torrent"]) ? (int)$_POST["torrent"] : 0;
 if (empty($torrent))
     die("Операция невозможна");
 
@@ -13,13 +13,23 @@ if(get_user_class() < UC_MODERATOR)
     die("Нет доступа");
 
 $res = sql_query("SELECT modded,  modby ,owner FROM torrents WHERE id = ".sqlesc($torrent)) or sqlerr(__FILE__,__LINE__);
-$row = mysql_fetch_assoc($res);
-$moder = $row['modby'];
-if ($row["modded"] == "no")
+$row = mysqli_fetch_assoc($res);
+if (!$row) {
+    die("Раздача не найдена");
+}
+
+$moder = (int)$row['modby'];
+if ($row["modded"] === "no")
     die("Раздача уже отправлена на проверку повторно !");
-$memcache_obj->delete('torrent_'.$torrent, 0);
+
+if ($memcache_obj instanceof Memcached) {
+    $memcache_obj->delete('torrent_' . $torrent, 0);
+}
+
 sql_query("UPDATE torrents SET modded = 'no', modby = ' ', modname = ' ', modtime = ' ' WHERE id = ".sqlesc($torrent)) or sqlerr(__FILE__,__LINE__);
-sql_query("UPDATE users SET moderated = moderated - 1 WHERE id = ".sqlesc($moder)) or sqlerr(__FILE__,__LINE__);
+if ($moder > 0) {
+    sql_query("UPDATE users SET moderated = moderated - 1 WHERE id = ".sqlesc($moder)) or sqlerr(__FILE__,__LINE__);
+}
 
 print "Раздача отправлена на проверку !";
 
